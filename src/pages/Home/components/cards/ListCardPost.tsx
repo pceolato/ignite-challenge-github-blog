eea5133f3a7c604f0f0cc8d41bed6ce50e626bc8
= <pedroceolato2002@gmail.com>
2023-02-14 08:01:51 -0300

    feat: add format pt in date and id issue

--- src/pages/Home/components/cards/ListCardPost.tsx
+++ src/pages/Home/components/cards/ListCardPost.tsx
@@ -1,18 +1,27 @@
 import { useBlog } from "../../../../hooks/useBlog";
-import { formatDistanceToNowStrict } from 'date-fns'
+import { formatDistanceToNowStrict } from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
 import { CardPost } from "./CardPost";
 
 export function ListCardPost() {
     const { issues } = useBlog()
 
-    
+
     return (
         <div className="flex flex-wrap gap-8 mt-12">
-            { issues.map(issue => {
-                const date = formatDistanceToNowStrict(Date.parse(issue.date))
-                
-                return <CardPost key={issue.title} title={issue.title} description={issue.content} date={date}/>
-                }) }
+            {issues.map(issue => {
+                const date = formatDistanceToNowStrict(Date.parse(issue.date), { locale: pt })
+
+                return (
+                    <CardPost
+                        id={issue.id}
+                        key={issue.id}
+                        title={issue.title}
+                        description={issue.content}
+                        date={date}
+                    />
+                )
+            })}
         </div>
     )
 }
