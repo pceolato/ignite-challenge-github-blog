fd2f941b3ab608f60271abef182aeda0aed56c34
= <pedroceolato2002@gmail.com>
2023-02-16 13:13:22 -0300

    style: ajust in list posts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import { Profile } from "./components/Profile";
 
 export function Home() {
     return (
-        <div className="max-w-[896px] mx-auto px-4">
+        <div className="max-w-[896px] mx-auto mb-8 px-4">
             <Profile />
             <Form />
             <ListCardPost />
