85f1585f5eaac89c2e7b76cad113be410100c008
= <pedroceolato2002@gmail.com>
2023-02-16 12:13:58 -0300

    feat: add content with react-markdown

--- src/pages/Home/components/cards/CardPost.tsx
+++ src/pages/Home/components/cards/CardPost.tsx
@@ -1,21 +1,32 @@
+import ReactMarkdown from "react-markdown";
 import { NavLink } from "react-router-dom";
 
 interface CardPostProps {
-    id: string;
+    id: number;
+    number: number;
     title: string;
     description: string;
     date: string
 }
 
-export function CardPost({ id, title, description, date }: CardPostProps) {
+export function CardPost({ id, title, description, date, number }: CardPostProps) {
+    const content = `${description.slice(0, 180)} ...`
+
     return (
-        <NavLink to={`posts/${id}`} className="w-full lg:w-[416px]  p-8 flex flex-col gap-5 bg-base-post rounded-[10px] border-2 border-transparent hover:border-2 hover:border-base-label">
+        <NavLink to={`posts/${number}`} className="w-full lg:w-[416px]  p-8 flex flex-col gap-5 bg-base-post rounded-[10px] border-2 border-transparent hover:border-2 hover:border-base-label">
             <div className="flex gap-4">
                 <h2 className="text-base-title font-bold text-xl flex-1">{title}</h2>
                 <span className="text-base-span text-sm">Há {date}</span>
             </div>
             <p className="text-base-text">
-                {description.slice(1, 180)}...
+            <ReactMarkdown
+                    components={{
+                        a: ({node, ...props}) => <a className="text-blue" {...props}></a>
+                    }}
+                    linkTarget="_blank"
+                >
+                {content}
+            </ReactMarkdown>
             </p>
         </NavLink>
     )
