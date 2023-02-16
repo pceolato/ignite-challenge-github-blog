8b8ffecd946835e5f8aa40c9db2e1081b91ab96a
= <pedroceolato2002@gmail.com>
2023-02-16 12:13:13 -0300

    feat: add post by id

--- src/contexts/BlogContext.tsx
+++ src/contexts/BlogContext.tsx
@@ -5,8 +5,10 @@ interface BlogProviderProps {
     children: ReactNode
 }
 
-interface IssuesType {
-    id: string
+interface PostsType {
+    id: number
+    user: string
+    number: number
     title: string
     content: string
     date: string
@@ -15,17 +17,20 @@ interface IssuesType {
 }
 
 interface BlogContextProps {
-    issues: IssuesType[]
-    totalIssues: number
+    posts: PostsType[]
+    totalPosts: number
+    postById: PostsType
+    fetchPostById: (id: number) => void;
 }
 
 export const BlogContext = createContext({} as BlogContextProps)
 
 export function BlogProvider({ children }: BlogProviderProps) {
-    const [ issues, setIssues ] = useState<IssuesType[]>([])
-    const [ totalIssues, setTotalIssues ] = useState(0)
+    const [ posts, setPosts ] = useState<PostsType[]>([])
+    const [ postById, setPostById] = useState({} as PostsType)
+    const [ totalPosts, setTotalPosts ] = useState(0)
 
-    async function fetchIssues() {
+    async function fetchAllPosts() {
         const { data }  = await api.get('/search/issues', {
             params: {
                 q:  'repo:pceolato/ignite-challenge-github-blog'
@@ -33,29 +38,46 @@ export function BlogProvider({ children }: BlogProviderProps) {
         }
         )
 
-        const issuesTotal:IssuesType[]  = []
+        const postsTotal:PostsType[]  = []
 
-        data.items.map((issue: any) => {
-            issuesTotal.push({
-                id: issue.id,
-                title: issue.title,
-                content: issue.body,
-                date: issue.created_at,
-                comments: issue.comments,
-                url: issue.html_url
+        data.items.map((post: any) => {
+            postsTotal.push({
+                id: post.id,
+                user: post.user.login,
+                number: post.number,
+                title: post.title,
+                content: post.body,
+                date: post.created_at,
+                comments: post.comments,
+                url: post.html_url
             })
         });
 
-        setIssues(issuesTotal)
-        setTotalIssues(data.total_count)
+        setPosts(postsTotal)
+        setTotalPosts(data.total_count)
+    }
+
+    async function fetchPostById(number: number) {
+        const { data }  = await api.get(`/repos/pceolato/ignite-challenge-github-blog/issues/${number}`)
+
+        setPostById({
+            id: data.id,
+            user: data.user.login,
+            number: data.number,
+            title: data.title,
+            content: data.body,
+            date: data.created_at,
+            comments: data.comments,
+            url: data.html_url
+        })
     }
 
     useEffect(() => {
-        fetchIssues()
+        fetchAllPosts()
     }, [])
 
     return (
-        <BlogContext.Provider value={{ issues, totalIssues }}>
+        <BlogContext.Provider value={{ posts, totalPosts, fetchPostById, postById }}>
             {children}
         </BlogContext.Provider>
     )
